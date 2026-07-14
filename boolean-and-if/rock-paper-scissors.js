6795ad62f7d54c4f28f5e2884d237a1eb77a3dfe
Ankith H S <ankith270600@gmail.com>
2026-07-14 23:22:35 +0530

    rock-paper-scissor js exercise

---
+++ boolean-and-if/rock-paper-scissors.js
@@ -0,0 +1,74 @@
+function choseRock(){
+    const randomNumber = Math.random();
+    let compMove = '';
+
+    if(randomNumber >= 0 && randomNumber < 1 / 3){
+        compMove = 'rock';
+    }
+    else if(randomNumber > 1/3 && randomNumber < 2/3){
+         compMove = 'paper';
+    }
+    else if(randomNumber > 2/3 && randomNumber < 1){
+         compMove = 'scissors';
+    }
+
+    let result = '';
+    if(compMove === 'rock'){
+        result = 'Tie';
+    } else if(compMove === 'paper'){
+        result = 'You lose'
+    } else if(compMove === 'scissors'){
+        result = 'You Win'
+    }
+    alert(`You picked rock.Computer picked ${compMove}. ${result}`);
+}
+
+function chosePaper(){
+    const randomNumber = Math.random();
+    let compMove = '';
+
+    if(randomNumber >= 0 && randomNumber < 1 / 3){
+        compMove = 'rock';
+    }
+    else if(randomNumber > 1/3 && randomNumber < 2/3){
+         compMove = 'paper';
+    }
+    else if(randomNumber > 2/3 && randomNumber < 1){
+         compMove = 'scissors';
+    }
+
+    let result = '';
+    if(compMove === 'rock'){
+        result = 'You Win';
+    } else if(compMove === 'paper'){
+        result = 'Tie'
+    } else if(compMove === 'scissors'){
+        result = 'You Lose'
+    }
+    alert(`You picked paper.Computer picked ${compMove}. ${result}`);
+}
+
+function choseSccisor(){
+    const randomNumber = Math.random();
+    let compMove = '';
+
+    if(randomNumber >= 0 && randomNumber < 1 / 3){
+        compMove = 'rock';
+    }
+    else if(randomNumber > 1/3 && randomNumber < 2/3){
+         compMove = 'paper';
+    }
+    else if(randomNumber > 2/3 && randomNumber < 1){
+         compMove = 'scissors';
+    }
+
+    let result = '';
+    if(compMove === 'rock'){
+        result = 'You Lose';
+    } else if(compMove === 'paper'){
+        result = 'You Win'
+    } else if(compMove === 'scissors'){
+        result = 'Tie'
+    }
+    alert(`You picked scissor.Computer picked ${compMove}. ${result}`);
+}
