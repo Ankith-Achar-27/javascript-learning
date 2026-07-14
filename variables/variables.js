04f8ef6e8e684b5540f291e9c2f2bd6e6ec1ae8a
Ankith H S <ankith270600@gmail.com>
2026-07-14 15:28:16 +0530

    intial commit

---
+++ variables/variables.js
@@ -0,0 +1,26 @@
+// Ways to create variable
+
+// 1. let  -> the variable can be reassigned again
+let var1 = 2;
+let var2 = 3;
+let cal = var1 + var2;
+console.log(cal);
+
+let message='Hello Ankith....';
+console.log(message);
+
+var1 = 10;
+cal = var1+var2;
+console.log(cal);
+
+// 2. const  -> once assigned can't be reassigned
+
+const v1 = 2;
+const v2 = 3;
+// v1 = 3; gives the error
+console.log(v1+v2);
+
+//3. var -> same as let can be changes but nowadays not used due to some issues
+
+var v=1;
+var vv=3;
