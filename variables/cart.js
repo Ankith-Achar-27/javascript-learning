397bf4531e553c57f40ff9fcc10542e1825ce78d
Ankith H S <ankith270600@gmail.com>
2026-07-14 16:42:31 +0530

    added function

--- variables/cart.js
+++ variables/cart.js
@@ -1,1 +1,25 @@
 let cartQuantity = 0;
+
+function showQuantity(){
+    console.log(`Cart quantity: ${cartQuantity}`);
+}
+
+function addOne(){
+        cartQuantity += 1;
+        console.log(`Cart quantity: ${cartQuantity}`);
+}
+
+function addTwo(){
+    cartQuantity +=2;
+    console.log(`Cart quantity: ${cartQuantity}`);
+}
+
+function addThree(){
+        cartQuantity += 3;
+        console.log(`Cart quantity: ${cartQuantity}`);
+}
+
+function reserCart(){
+     cartQuantity=0; console.log(`Cart Reset`);
+    console.log(`Cart quantity: ${cartQuantity}`);
+}
