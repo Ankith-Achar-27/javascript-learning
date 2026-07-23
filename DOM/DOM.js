9ce660dbe02e40789edaaa76736ce9386f1f1483
Ankith H S <ankith270600@gmail.com>
2026-07-23 21:45:46 +0530

    DOM js

---
+++ DOM/DOM.js
@@ -0,0 +1,29 @@
+// DOM(Document Object Model) = built in object by js
+// DOM combines html and js together
+
+/*document.body.innerHTML = 'hello';
+document.title = 'Good job.!';*/
+/*
+console.log(document.title);
+document.title = 'changed';
+
+console.log(document.body);
+console.log(typeof document.body); //object
+
+console.log(document.body.innerHTML);
+document.body.innerHTML = '<button>Good Job</button>';
+*/
+
+// method is a function saved inside an object
+
+ //document.querySelector() ->
+ // lets us get any element from the page and put it inside javascript
+
+console.log(document.querySelector('button').innerHTML);
+
+document.querySelector('button')
+    .innerHTML = 'Changed';
+
+// start with . to look for class
+const buttonElement = document.querySelector('.js-button');
+console.log(buttonElement);
