84f3191fa3ca80b027fee7bf3033946d0680b3a4
Ankith H S <ankith270600@gmail.com>
2026-07-26 21:31:50 +0530

    deleted

--- objects/calculator.js
+++
@@ -1,8 +0,0 @@
-let calculation = '';
-calculation = JSON.parse(localStorage.getItem('calculation')) || '';
-
-function updateCalculation(value) {
-    calculation += value;
-    console.log(calculation);
-    localStorage.setItem('calculation', JSON.stringify(calculation));
-}
