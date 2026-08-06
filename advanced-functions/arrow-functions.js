27f7f94aa4ad69586be7a83b9f9245182956d1f1
Ankith H S <ankith270600@gmail.com>
2026-08-06 21:18:32 +0530

    added .filter() and .map()

--- advanced-functions/arrow-functions.js
+++ advanced-functions/arrow-functions.js
@@ -1,4 +1,4 @@
-
+// forEach Loop
 ['apple','banana','mango','pear'].forEach((value,index) =>{
     console.log(value);
     console.log(index);
@@ -34,3 +34,22 @@ buttonElement.removeEventListener('click',eventListener);
 buttonElement.addEventListener('click',()=>{
     console.log('clicked2')}
 );
+
+// .filter()
+console.log([1,2,-4,5].filter((value,index)=>{
+    /*if(value>=0){
+        return true; // puts the value in new array
+    }
+    else{
+        return false;
+    } */
+// short hand
+    return value>=0;
+}));
+
+// [1,1,2] double the element in the array
+
+/*console.log([1,1,2].map((value,index)=>{
+    return 2*value;
+})); //[2,2,4] */
+console.log([1,1,2].map(value=>{ return value*2;}));
