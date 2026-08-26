6eee6a50bb54d6397d0923ca026d9eaa95c49726
Ankith H S <ankith270600@gmail.com>
2026-08-26 22:02:42 +0530

    added updateDeliveryOption id

--- amazon-project/data/cart.js
+++ amazon-project/data/cart.js
@@ -47,4 +47,15 @@ export function removeCartItem(productId){
     });
     cart = newCart;
     saveToStorage();
-}
+};
+
+export function updateDeliveryOption(productId,deliveryOptionId){
+    let matchingItem;
+        cart.forEach((cartItem)=>{
+            if(productId === cartItem.productId){
+                matchingItem=cartItem;
+            }
+        });
+    matchingItem.deliveryOptionId = deliveryOptionId;
+    saveToStorage();
+};
